1244fc17ffee7907864ae56d04ed0fde165638b1
이창한 <momo0170@naver.com>
2024-03-24 19:09:37 +0900

    feature: 회원가입 기능 추가
      - 사용자 입력한 정보를 임시 DB(userStorage)에 저장

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import diaryRouter from './router/diary.js';
 
-const app = express();
+let userStorage = []; // 임시 사용자 DB
 
+const app = express();
 app.use(express.json()); // req.body를 읽기 위함
 app.use(cors());
 app.use('/diary', diaryRouter);
+app.post('/sign-up', (req, res, next) => {
+  console.log(req.body);
+  const { id, name, password, nickname, url } = req.body;
+  const newUser = {
+    id,
+    password,
+    name,
+    nickname,
+    url,
+  };
+  // 새로운 사용자를 userStorage에 추가
+  userStorage = [...userStorage, newUser];
+  console.log(userStorage);
+  res.status(201).json(userStorage);
+});
 
 // 모든 라우터가 처리가 안될 시
 app.use((req, res, next) => {
