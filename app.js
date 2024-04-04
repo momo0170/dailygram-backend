6ffb1a648688032cbee6b12668f4d16fd2cd2c0c
이창한 <momo0170@naver.com>
2024-04-04 10:00:03 +0900

    style: 콘솔 로그 제거 및 임시 사용자 데이터 추가

--- app.js
+++ app.js
@@ -2,14 +2,42 @@ import express from 'express';
 import cors from 'cors';
 import diaryRouter from './router/diary.js';
 
-let userStorage = []; // 임시 사용자 DB
+let userStorage = [
+  {
+    id: 'abc123',
+    password: 'abc123',
+    name: '창한',
+    nickname: '홍길동',
+    url: 'https://images.unsplash.com/photo-1710942499889-71f233dae342?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8',
+  },
+  {
+    id: 'ab12',
+    password: 'ab12',
+    name: '동현',
+    nickname: '동태',
+    url: 'https://images.unsplash.com/photo-1710942499889-71f233dae342?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8',
+  },
+  {
+    id: 'momo0170',
+    password: 'momo0170',
+    name: '민성',
+    nickname: 'queit',
+    url: 'https://images.unsplash.com/photo-1707343843344-011332037abb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8',
+  },
+  {
+    id: 'h33265571',
+    password: 'h33265571',
+    name: '지현',
+    nickname: '지효니',
+    url: 'https://images.unsplash.com/photo-1711843250893-21a3f56624ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D',
+  },
+]; // 임시 사용자 DB
 
 const app = express();
 app.use(express.json()); // req.body를 읽기 위함
 app.use(cors());
 app.use('/diary', diaryRouter);
 app.post('/sign-up', (req, res, next) => {
-  console.log(req.body);
   const { id, name, password, nickname, url } = req.body;
   const newUser = {
     id,
@@ -20,7 +48,6 @@ app.post('/sign-up', (req, res, next) => {
   };
   // 새로운 사용자를 userStorage에 추가
   userStorage = [...userStorage, newUser];
-  console.log(userStorage);
   res.status(201).json({ users: userStorage, currentUser: newUser });
 });
 
